df62d978d9723f9bce54cc467e22d9667ac4d04e
Dauta <idauta@gmail.com>
2019-05-20 03:59:08 +0200

    add content to info pages + add images + favicon

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,9 @@
-function restrictScrolling(e) {
-  // if (e.deltaY <= 0) return 0;
+const downloadUrl = 'https://docs.google.com/viewer?url=https://docs.google.com/document/d/1em5v-nLuPFSExcal91TA8FjQbM9ci4_myZnejeFIxSk/export?format=pdf';
 
-  return e.deltaY / 1.5;
-}
-
-function offsetLimit() {
-  const body = document.body;
-  return body.offsetHeight / 10;
-}
-
-function moveRocket(shift) {
-  const rocket = document.querySelector('.rocket-container');
-  let offset = window.getComputedStyle(rocket).transform.split(',')[5];
-
-  if(offset) {
-    offset = offset.split(')')[0];
-    offset = Math.abs(parseInt(Math.floor(offset)));
-  } else {
-    offset = 0;
-  }
-
-  if(offset > offsetLimit()) return;
-
-  rocket.style.transform = `translate(0, -${(shift + offset)}px)`;
-}
-
-(function init() {
-  window.addEventListener('wheel', (e) => {
-    e.stopImmediatePropagation();
-    const scrollVal = restrictScrolling(e);
-
-    moveRocket(scrollVal);
-  }, true)
-})();
+document.addEventListener('DOMContentLoaded', () => {
+  // config download button
+  const downloadButton = document.querySelector('.menu-item__download');
+  downloadButton.addEventListener('click', () => {
+    window.open(downloadUrl, 'blank');
+  });
+});
