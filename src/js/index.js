341454822fabe52cab8eb73add4fce6a150d3a6d
Irakli Dautashvili <idauta@gmail.com>
2022-06-22 18:12:33 +0200

    update CV link

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-const downloadUrl = 'https://docs.google.com/viewer?url=https://docs.google.com/document/d/1em5v-nLuPFSExcal91TA8FjQbM9ci4_myZnejeFIxSk/export?format=pdf';
-
-document.addEventListener('DOMContentLoaded', () => {
-  // config download button
-  const downloadButton = document.querySelector('.menu-item__download');
-  downloadButton.addEventListener('click', () => {
-    window.open(downloadUrl, 'blank');
-  });
-});
